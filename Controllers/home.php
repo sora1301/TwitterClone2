857bb39cc09ae5fe24f21856ac6f84ca40b14c2f
sora1301 <sorajiumu@gmail.com>
2024-09-01 16:33:12 +0900

    ホーム機能

--- Controllers/home.php
+++ Controllers/home.php
@@ -7,21 +7,20 @@
 include_once '../config.php';
 // 便利な関数を読み込み
 include_once '../util.php';
-
-//ツイートデータ操作もでるを読み込む
-include_once '../Models/tweets.php'
-
-// ログインチェック
+ 
+// ツイートデータ操作モデルを読み込む
+include_once '../Models/tweets.php';
+ 
+// ログインしているか
 $user = getUserSession();
 if (!$user) {
     // ログインしていない
     header('Location: ' . HOME_URL . 'Controllers/sign-in.php');
     exit;
 }
-
-// 表示用の変数
+ 
+// 画面表示
 $view_user = $user;
 // ツイート一覧
 $view_tweets = findTweets($user);
-//画面表示
 include_once '../Views/home.php';
